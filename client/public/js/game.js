73e47144fd3756a54a67e7bf2795ae36655f5ab4
r-hoff <ryanchoff@gmail.com>
2023-02-04 15:14:47 -0800

    add health/shield ui elements

--- client/public/js/game.js
+++ client/public/js/game.js
@@ -3,6 +3,7 @@ function addPlayer(self, playerInfo) {
     self.ship.setDrag(100);
     self.ship.setAngularDrag(100);
     self.ship.setMaxVelocity(200);
+    self.ship.setCollideWorldBounds(true);
 }
 
 function addOtherPlayers(self, playerInfo) {
@@ -13,20 +14,61 @@ function addOtherPlayers(self, playerInfo) {
 
 class Game extends Phaser.Scene {
     constructor() {
-        super();
+        super({ key: 'GameScene' });
         this.bullets;
         this.ship;
+        this.player;
     }
 
     preload() {
+        // UI elements
+        this.load.image('bg', 'assets/bg.jpg');
+        this.load.image('healthIcon', 'assets/healthIcon.png');
+        this.load.image('shieldIcon', 'assets/shieldIcon.png');
+        this.load.image('healthNode', 'assets/healthNode.png');
+        this.load.image('shieldNode', 'assets/shieldNode.png');
+        // game elements
         this.load.image('ship', 'assets/spaceShips_001.png');
         this.load.image('otherPlayer', 'assets/enemyBlack5.png');
         this.load.image('bullet', 'assets/purple_ball.png');
     }
 
     create() {
+        // load background image and scale to size of screen
+        let background = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'bg');
+        let scale = Math.max(this.cameras.main.width / background.width, this.cameras.main.height / background.height);
+        background.setScale(scale).setScrollFactor(0);
+
         const self = this;
-        this.socket = io("http://localhost:9000");
+        this.socket = io('http://localhost:9000');
+
+        // get player's upgrade level (will need to be replaced with a db query, from the player entity)
+        // player upgrade level will be 0-5 for both health and shield
+        this.player = {
+            healthUpgrade: 3,
+            shieldUpgrade: 1,
+        }
+        // initialize the player's upgrades with the server
+        this.socket.emit('initialize', this.player);
+
+        this.ui = this.physics.add.group();
+        // initialize the UI with player starting stats
+        this.socket.on('initUi', player => {
+            let pos = 50;
+            // this.ui.add(10, 10, 'healthIcon').setOrigin(0).setScale(0.4);
+            this.ui.add(this.add.image(10, 10, 'healthIcon').setOrigin(0).setScale(0.4));
+            for (let i = 0; i < player.health; i++) {
+                this.ui.add(this.add.image(pos, 12.5, 'healthNode').setOrigin(0).setScale(0.5));
+                pos += 15;
+            }
+            this.ui.add(this.add.image(10, 50, 'shieldIcon').setOrigin(0).setScale(0.4));
+            pos = 50;
+            for (let i = 0; i < player.shield; i++) {
+                this.ui.add(this.add.image(pos, 52.5, 'shieldNode').setOrigin(0).setScale(0.5));
+                pos += 15;
+            }
+        });
+
         this.otherPlayers = this.physics.add.group();
         this.bullets = new Bullets(this);
 
@@ -71,7 +113,6 @@ class Game extends Phaser.Scene {
 
     update() {
         if (this.ship) {
-
             // emit player movement
             let x = this.ship.x;
             let y = this.ship.y;
