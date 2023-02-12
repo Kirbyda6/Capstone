780fdcd8597838ab82f46ede7c31717f6357dfb7
r-hoff <ryanchoff@gmail.com>
2023-02-11 17:38:45 -0800

    update title

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -7,13 +7,14 @@ class Main extends Phaser.Scene {
 
     preload() {
         // UI elements
-        this.load.image('playButton', '../assets/playButton.png')
+        this.load.image('playButton', '../assets/playButton.png');
+        this.load.image('gameTitle', '../assets/gameTitle.png');
     }
 
     create() {
         const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
         const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
-        this.add.text(screenCenterX, screenCenterY - 200, 'Galactic Gauntlet', { fontFamily: 'Courier', fontSize: '64px' }).setOrigin(0.5);
+        this.add.image(screenCenterX, screenCenterY - 200, 'gameTitle').setOrigin(0.5);
 
         let button = this.add.image(screenCenterX, screenCenterY, 'playButton').setInteractive({ cursor: 'pointer' });
         button.on('pointerdown', () => { this.scene.start('GameScene') });
