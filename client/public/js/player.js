913f1717bc1933b09e947c4a79696439fd224f4f
r-hoff <ryanchoff@gmail.com>
2023-02-26 15:47:14 -0800

    add upgrade shop functions

--- client/public/js/player.js
+++ client/public/js/player.js
@@ -31,4 +31,40 @@ export class Player {
             return response.json()
         })
     }
+
+    updateHealth(player) {
+        return fetch(`http://localhost:9000/player/${player.id}/health`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + player.jwt
+            }
+        }).then((response) => {
+            return response.json()
+        })
+    }
+
+    updateShields(player) {
+        return fetch(`http://localhost:9000/player/${player.id}/shields`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + player.jwt
+            }
+        }).then((response) => {
+            return response.json()
+        })
+    }
+
+    updateSpeed(player) {
+        return fetch(`http://localhost:9000/player/${player.id}/speed`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + player.jwt
+            }
+        }).then((response) => {
+            return response.json()
+        })
+    }
 }
