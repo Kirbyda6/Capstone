1c26dc907f0dd843c6b14923e6ceff9f5dc1e750
r-hoff <ryanchoff@gmail.com>
2023-03-05 12:27:08 -0800

    multiplayer bullet fix

--- client/public/js/bullet.js
+++ client/public/js/bullet.js
@@ -6,30 +6,34 @@ export class Bullet extends Phaser.Physics.Arcade.Sprite {
             this.scene.physics.add.collider(this.scene.otherPlayers, this, (bullet, otherPlayer) => {
                 this.scene.socket.emit('damagePlayer', otherPlayer.socketId, 'player', this.scene.socket.id);
                 this.scene.hit.play();
-                bullet.destroy();
+                bullet.setActive(false).setVisible(false);
+                bullet.body.enable = false;
             });
             this.scene.physics.add.collider(this.scene.aiEnemies, this, (bullet, enemy) => {
                 this.scene.socket.emit('enemyDied', enemy.enemyId, this.scene.socket.id);
                 this.scene.alienSound.play();
                 enemy.destroy();
-                bullet.destroy();
+                bullet.setActive(false).setVisible(false);
+                bullet.body.enable = false;
             });
         } else if (key === 'enemyBullet') {
             this.scene.physics.add.collider(this.scene.ship, this, (ship, bullet) => {
                 this.scene.hit.play();
-                bullet.destroy();
+                bullet.setActive(false).setVisible(false);
+                bullet.body.enable = false;
             });
             this.scene.physics.add.collider(this.scene.aiEnemies, this, (bullet, enemy) => {
                 this.scene.alienSound.play();
                 enemy.destroy();
-                bullet.destroy();
+                bullet.setActive(false).setVisible(false);
+                bullet.body.enable = false;
             });
         }
     }
 
     fire(ship) {
         this.body.reset(ship.x, ship.y);
-
+        this.body.enable = true;
         this.setActive(true);
         this.setVisible(true);
 
